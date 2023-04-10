ee2ad9465127abf60dcf83c3ee8d5d60a15e6cca
CoderTvisha <71917050+CoderTvisha@users.noreply.github.com>
2023-04-10 17:23:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+rightWristX = 0;
+leftWristX = 0;
+difference = 0;
+noseX = "";
+noseY = "";
+var n1 = "";
+
+function setup() {
+    canvas = createCanvas(550, 450);
+    canvas.position(560, 150);
+    video = createCapture(VIDEO);
+    video.size(550, 550);
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('poseNet is initialised');
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+        console.log("right wrist = " + rightWristX + "left wrist = " + leftWristX + "difference = " + difference);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+    }
+}
+
+function name1(){
+    n1 = document.getElementById("name_input").value;
+    console.log(n1);
+}
+
+function draw() {
+
+    
+
+    background('#b4d6c4');
+    fill('white');
+    stroke('black');
+    textSize(difference);
+    text(n1, noseX, noseY);
+
+}
